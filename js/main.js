be5d5be5e4fd948d94b57a89f8c2bfb3ed24cafa
cartrugs <cartrugs@gmail.com>
2023-07-08 19:48:38 +0200

    Desarrollo de evento para comprobar y función para comprobación

--- js/main.js
+++ js/main.js
@@ -1,52 +1,98 @@
-// Variables 
+// VARIABLES
 
 // querySelector
 const matriculaInput = document.querySelector('#matriculaInput');
+
 const btnComprobar = document.querySelector('#btnComprobar');
 const dataTable = document.querySelector('#dataTable');
 
 // Array de objetos para almacenar matrículas y datos
 let matriculasArray = [
     {
+        id: '1',
         matricula: '7834JKM',
         modelo: 'Toyota CH-R Hybrid,',
         propietario: 'María OBrian Blü',
         multas: '',
     },
     {
-        matricula: '7834JKM',
-        modelo: 'Toyota CH-R Hybrid,',
-        propietario: 'María OBrian Blü',
+        id: '2',
+        matricula: '8725JFDN',
+        modelo: 'HYUNDAI i-20,',
+        propietario: 'Juan Fernández-Mar',
+        multas: '3',
+    },
+    {
+        id: '3',
+        matricula: '9381POO',
+        modelo: 'Toyota Auris,',
+        propietario: 'Sofía Gómez Addams',
         multas: '',
     },
     {
-        matricula: '7834JKM',
-        modelo: 'Toyota CH-R Hybrid,',
-        propietario: 'María OBrian Blü',
-        multas: '',
+        id: '4',
+        matricula: '5690MNB',
+        modelo: 'KIA Sportage,',
+        propietario: 'Marta Reina Flor',
+        multas: '2',
     },
     {
-        matricula: '7834JKM',
+        id: '5',
+        matricula: '8216JMN',
         modelo: 'Toyota CH-R Hybrid,',
-        propietario: 'María OBrian Blü',
-        multas: '',
+        propietario: 'Sebastián Bolardos Volados',
+        multas: '6',
     },
     {
-        matricula: '7834JKM',
-        modelo: 'Toyota CH-R Hybrid,',
-        propietario: 'María OBrian Blü',
-        multas: '',
-    },
-    {
-        matricula: '7834JKM',
-        modelo: 'Toyota CH-R Hybrid,',
-        propietario: 'María OBrian Blü',
-        multas: '',
+        id: '6',
+        matricula: '4518KWH',
+        modelo: 'Renault Laguna,',
+        propietario: 'Francisco Reyes Alonso',
+        multas: '7',
     }
-]
+];
+
 // Expresiones regulares
 const regEx = {
     matricula: /^[0-9]{4}[A-Z]{3}$/,
     modelo: /^[a-z0-9\- ]{1,30}$/i,
     propietario: /^[a-zA-ZÑ-ÿ\s]{1,50}$/
 };
+
+
+// fragment
+const fragment = document.createDocumentFragment();
+
+// EVENTOS
+
+// Botón comprobar
+document.addEventListener('click', ({target}) => {
+    if (target.classList.contains('check-button')) {
+        const valoresObjMatriculas = target.id;
+        comprobacionMatriculaFunc(valoresObjMatriculas);
+    }
+})
+
+// FUNCIONES
+
+// Almacenar localStorage
+const alamcenarLocal = (id) => {
+
+};
+
+// Funcion comprobar 
+const comprobacionMatriculaFunc = (matricula) => {
+    const matriculaEncontrada = matriculasArray.find((item) => item.matricula === matricula);
+    return new Promise((resolve, reject) => {
+      if (matriculaEncontrada) {
+        if (matriculaEncontrada.multas !== '') {
+          resolve(true);
+        } else {
+          reject(`La matrícula ${matricula} no tiene multas.`);
+        }
+      } else {
+        reject(`La matrícula ${matricula} no existe.`);
+      }
+    });
+  };
+  
