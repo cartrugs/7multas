728f13f3fb65b165c1293ce6f79a581d487a4304
cartrugs <cartrugs@gmail.com>
2023-07-15 19:43:06 +0200

    Input comprobar arreglado, ya funciona correctamente. Cambios en html, eliminación de id innecesarios

--- js/main.js
+++ js/main.js
@@ -4,137 +4,134 @@
 const matriculaInput = document.querySelector('#matriculaInput');
 const btnComprobar = document.querySelector('#btnComprobar');
 const dataTable = document.querySelector('#dataTable');
-const mensajeMaticulas = document.querySelector('#mensaje-matriculas');
+const mensajeMatriculas = document.querySelector('#mensaje-matriculas');
 // Variable para nombrar clave en localStorage
-const claveLocal = 'checkInLocal'; 
+const claveLocal = 'checkInLocal';
 
 // Array de objetos para almacenar matrículas y datos
 let matriculasArray = [
-    {
-        id: '1',
-        matricula: '7834JKM',
-        modelo: 'Toyota CH-R Hybrid,',
-        propietario: 'María OBrian Blü',
-        multas: '',
-    },
-    {
-        id: '2',
-        matricula: '8725JFD',
-        modelo: 'HYUNDAI i-20,',
-        propietario: 'Juan Fernández-Mar',
-        multas: '3',
-    },
-    {
-        id: '3',
-        matricula: '9381POO',
-        modelo: 'Toyota Auris,',
-        propietario: 'Sofía Gómez Addams',
-        multas: '',
-    },
-    {
-        id: '4',
-        matricula: '5690MNB',
-        modelo: 'KIA Sportage,',
-        propietario: 'Marta Reina Flor',
-        multas: '2',
-    },
-    {
-        id: '5',
-        matricula: '8216JMN',
-        modelo: 'Toyota CH-R Hybrid,',
-        propietario: 'Sebastián Bolardos Volados',
-        multas: '6',
-    },
-    {
-        id: '6',
-        matricula: '4518KWH',
-        modelo: 'Renault Laguna,',
-        propietario: 'Francisco Reyes Alonso',
-        multas: '7',
-    }
+  {
+    id: '1',
+    matricula: '7834JKM',
+    modelo: 'Toyota CH-R Hybrid',
+    propietario: 'María OBrian Blü',
+    multas: '',
+  },
+  {
+    id: '2',
+    matricula: '8725JFD',
+    modelo: 'HYUNDAI i-20',
+    propietario: 'Juan Fernández-Mar',
+    multas: '3',
+  },
+  {
+    id: '3',
+    matricula: '9381POO',
+    modelo: 'Toyota Auris',
+    propietario: 'Sofía Gómez Addams',
+    multas: '',
+  },
+  {
+    id: '4',
+    matricula: '5690MNB',
+    modelo: 'KIA Sportage',
+    propietario: 'Marta Reina Flor',
+    multas: '2',
+  },
+  {
+    id: '5',
+    matricula: '8216JMN',
+    modelo: 'Toyota CH-R Hybrid',
+    propietario: 'Sebastián Bolardos Volados',
+    multas: '6',
+  },
+  {
+    id: '6',
+    matricula: '4518KWH',
+    modelo: 'Renault Laguna',
+    propietario: 'Francisco Reyes Alonso',
+    multas: '7',
+  },
 ];
-
 
 // Expresiones regulares
 const regEx = {
-    matricula: /^[0-9]{4}[A-Z]{3}$/,
-    modelo: /^[a-z0-9\- ]{1,30}$/i,
-    propietario: /^[a-zA-ZÑ-ÿ\s]{1,50}$/
+  matricula: /^[0-9]{4}[A-Z]{3}$/,
+  modelo: /^[a-z0-9\- ]{1,30}$/i,
+  propietario: /^[a-zA-ZÑ-ÿ\s]{1,50}$/,
 };
 
 // fragment
 const fragment = document.createDocumentFragment();
 
-// Variable para recuperar localStorage. Tras el operador OR se proporciona un valor predeterminado en caso de que el  localStorage sea NULL.
+// Variable para recuperar localStorage. Tras el operador OR se proporciona un valor predeterminado en caso de que el localStorage sea NULL.
 const recuperarLocal = JSON.parse(localStorage.getItem(claveLocal)) || [];
 
-// Variable para recuperar localStorage
-// const recuperarLocal = JSON.parse(localStorage.getItem(claveLocal));
 // EVENTOS
 
 // Botón comprobar
-document.addEventListener('click', ({ target }) => {
-  if (target.classList.contains('check-button')) {
-    const valoresObjMatriculas = matriculasArray.matricula;
-    comprobacionMatriculaFunc(valoresObjMatriculas)
-      .then((resp) => {
-        const mensajeMatriculas = document.getElementById('mensaje-matriculas');
-        mensajeMatriculas.textContent = resp;
-      })
-      .catch((error) => {
-        const mensajeMatriculas = document.getElementById('mensaje-matriculas');
-        mensajeMatriculas.textContent = error;
-      });
-  }
+btnComprobar.addEventListener('click', () => {
+  const matricula = matriculaInput.value;
+  comprobacionMatriculaFunc(matricula)
+    .then((resp) => {
+      mensajeMatriculas.textContent = resp;
+      crearTablaPintada();
+    })
+    .catch((error) => {
+      mensajeMatriculas.textContent = error;
+      crearTablaPintada();
+    });
 });
-
-
 
 // FUNCIONES
 
 // Función almacenar localStorage
 const almacenarLocal = (id) => {
-  localStorage.setItem('claveLocal',JSON.stringify(claveLocal));
-
+  localStorage.setItem(claveLocal, JSON.stringify(id));
 };
 
-// Función comprobar 
+// Función comprobar
 const comprobacionMatriculaFunc = (matricula) => {
-    const matriculaEncontrada = matriculasArray.find((item) => item.matricula === matricula);
-    return new Promise((resolve, reject) => {
-      if (matriculaEncontrada) {
-        if (matriculaEncontrada.multas !== '') {
-          resolve(`La matricula ${matricula} tiene ${multas}`);
-          localStorage.setItem('claveLocal',JSON.stringify(claveLocal));
-        } else {
-          reject(`La matrícula ${matricula} no tiene multas.`);
-          localStorage.setItem('claveLocal',JSON.stringify(claveLocal));
-        }
+  const matriculaEncontrada = matriculasArray.find(
+    (item) => item.matricula === matricula
+  );
+  return new Promise((resolve, reject) => {
+    if (matriculaEncontrada) {
+      if (matriculaEncontrada.multas !== '') {
+        resolve(
+          `La matrícula ${matriculaEncontrada.matricula} tiene ${matriculaEncontrada.multas} multas.`
+        );
       } else {
-        reject(`La matrícula ${matricula} no existe.`);
-        localStorage.setItem('claveLocal',JSON.stringify(claveLocal));
+        resolve(
+          `La matrícula ${matriculaEncontrada.matricula} no tiene multas.`
+        );
       }
-      return matriculaEncontrada
-    })
-    
-  };
-
-// Función pintar
-const crearTablaPintada = () => {
-  dataTable.innerHTML = '';
-  recuperarLocal.forEach((item) => {
-    const filaNueva = document.createElement('TR');
-    const celdaMatricula = document.createElement('TD');
-    celdaMatricula.textContent = matricula.matricula;
-    const celdaModelo = document.createElement('TD');
-    celdaModelo.textConetnt = matricula.modelo;
-    const celdaPropietario = document.createElement('TD');
-    celdaPropietario.textContent = matricula.propietario;
-    const celdaMultas = document.createElement('TD');
-    celdaPropietario.textContent = matricula.multas;
-
-    filaNueva.append(celdaMatricula, celdaModelo, celdaPropietario, celdaMultas);
-    
+    } else {
+      reject(`La matrícula ${matricula} no existe.`);
+    }
   });
-  fragment.append(filaNueva);
 };
+
+const crearTablaPintada = () => {
+  if (dataTable) {
+    dataTable.innerHTML = '';
+    recuperarLocal.forEach((matricula) => {
+      const filaNueva = document.createElement('tr');
+      const celdaMatricula = document.createElement('td');
+      celdaMatricula.textContent = matricula.matricula;
+      const celdaModelo = document.createElement('td');
+      celdaModelo.textContent = matricula.modelo;
+      const celdaPropietario = document.createElement('td');
+      celdaPropietario.textContent = matricula.propietario;
+      const celdaMultas = document.createElement('td');
+      celdaMultas.textContent = matricula.multas;
+
+      filaNueva.append(celdaMatricula, celdaModelo, celdaPropietario, celdaMultas);
+      fragment.append(filaNueva);
+    });
+    dataTable.append(fragment);
+  }
+};
+
+
+
